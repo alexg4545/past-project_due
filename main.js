bd579390f000c379d3f82b2974aeb657270fcd99
alexg4545 <136864636+alexg4545@users.noreply.github.com>
2023-12-12 15:28:49 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+setInterval(() => {
+var first_name = "Alex";
+var mid_name = "Samuel";
+var last_name = "Barnes";
+
+var a = Math.PI;
+var b = Math.floor(Math.random(a));
+
+var connect = [first_name, mid_name, last_name, a, b];
+
+console.log(connect);
+}, 2000);
+
+setInterval(() => {
+    console.clear();
+}, 4000);
